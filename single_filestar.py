bd609caf9d283b9f1e5324b912ab8b59cd0fee24
MonadWizard <monad.wizard.r@gmail.com>
2023-10-25 12:47:17 +0600

    25_oct_2023

--- single_filestar.py
+++ single_filestar.py
@@ -1,27 +1,17 @@
 import requests
-import json
 
-# Read credentials from the config file
-try:
-    with open("config.json") as config_file:
-        config = json.load(config_file)
+# Your GitHub username and personal access token
+github_username = "YourGitHubUsername"
+personal_access_token = "YourPersonalAccessToken"
 
-    github_username = config["github_username"]
-    personal_access_token = config["personal_access_token"]
-    target_username = config["target_username"]
-except FileNotFoundError:
-    print("Error: config.json not found. Make sure the file exists in the same directory as the script.")
-    exit(1)
-except KeyError:
-    print("Error: Missing or incorrect fields in config.json. Please ensure the file is correctly formatted.")
-    exit(1)
+# The GitHub username whose public repositories you want to star
+target_username = "TargetGitHubUsername"
 
 # Set up the request headers with your personal access token
 headers = {
     "Authorization": f"token {personal_access_token}",
     "Accept": "application/vnd.github.v3+json"
 }
-
 
 # Define a function to star all public repositories of the target user
 def star_all_public_repositories():
@@ -52,11 +42,8 @@ def star_all_public_repositories():
 
             page += 1
         else:
-            print(
-                f"Failed to fetch public repositories for {target_username}. Status code: {response.status_code}"
-            )
+            print(f"Failed to fetch public repositories for {target_username}. Status code: {response.status_code}")
             break
-
 
 if __name__ == "__main__":
     star_all_public_repositories()
